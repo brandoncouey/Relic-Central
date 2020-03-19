4dd8f4ba290721ab4ba17258bfdb4e77b7d5d5dd
GrizzlyEntertainment <47308540+GrizzlyEntertainment@users.noreply.github.com>
2020-03-18 22:16:55 -0500

    Updated Central lib.

--- pushdocker.cmd
+++ pushdocker.cmd
@@ -1,3 +1,3 @@
-docker build -t central .
-docker tag central:latest 344855247956.dkr.ecr.us-east-2.amazonaws.com/central:latest
-docker push 344855247956.dkr.ecr.us-east-2.amazonaws.com/central:latest
+docker build -t grizzlyentertainment/relic-central:latest .
+docker push grizzlyentertainment/relic-central:latest
+pause
